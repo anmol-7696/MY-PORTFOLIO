f57d882e3ca276df1adf9638aa3f1bd08e0ed412
Anmolpreet <anmol7696ss@gmail.com>
2023-12-26 14:14:43 -0600

    dark mode added to the portfolio

--- file.js
+++ file.js
@@ -1,1 +1,12 @@
-console.log("Helllo! World.")
+document.addEventListener('DOMContentLoaded', function () {
+    const body = document.body;
+    const darkModeToggle = document.createElement('div');
+    darkModeToggle.className = 'dark-mode-toggle';
+    darkModeToggle.innerHTML = 'Toggle Dark Mode';
+
+    darkModeToggle.addEventListener('click', function () {
+        body.classList.toggle('dark-mode');
+    });
+
+    document.body.appendChild(darkModeToggle);
+});
