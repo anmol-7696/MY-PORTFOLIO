c1f537e1ae37a43d631fa961410c7918600d7116
Anmolpreet <anmol7696ss@gmail.com>
2024-07-20 19:23:37 -0500

    light dark mode

---
+++ file.js
@@ -0,0 +1,7 @@
+document.getElementById('checkbox').addEventListener('change', function(event){
+    if(event.target.checked) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  });
